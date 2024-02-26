b4969d210ece638040f977aff49226b2f720d4db
Thao Trinh <dttrinh96@gmail.com>
2024-02-25 20:37:07 -0800

    fix font size

---
+++ dev/frontend/src/components/dashboard/Dashboard.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from "react";
+import deleteIcon from "../../icons/delete.png"
+import pictureIcon from "../../icons/picture.png"
+
+const Dashboard = (props) => {
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    const [startTime, setStartTime] = useState(props.formattedDate);
+    const [endTime, setEndTime] = useState(props.formattedDate);
+
+    useEffect = () => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                setLat(position.coords.latitude);
+                setLng(position.coords.longitude);
+            },
+            error => console.log(error)
+        );
+    }
+
+    const handleLocationClick = () => {
+        const options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        };
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+        } else {
+            console.log("Geolocation not supported")
+        }
+    }
+
+    const handleStartTimeChange = (event) => {
+        setStartTime(event.target.value);
+    }
+
+    const handleEndTimeChange = (event) => {
+        setEndTime(event.target.value);
+    }
+
+    const handleSettingsClick = () => {
+
+    }
+
+    const errorCallback = () => {
+        console.log("Cannot retrieve the current location")
+    }
+
+    const successCallback = (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        console.log(`Latitude: ${latitude}, longitude: ${longitude}`)
+    }
+
+    return (
+        <section className="text-gray-400 bg-gray-900 body-font relative flex-grow">
+            <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <iframe width="100%" height="100%" title="map" className="absolute inset-0" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" allow="geolocation" src={`https://maps.google.com/maps?q=${lat},${lng}&z=15&output=embed`} style={{ filter: '' }}></iframe>
+                </div>
+                <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-white text-2xl mb-1 font-medium title-font">Create New Event</h2>
+                    <p className="leading-relaxed mb-5 text-lg text-blue-600"><button onClick={handleLocationClick}>Get Current Location</button></p>
+                    <div className="relative mb-4">
+                        <label for="code" className="leading-7 text-lg text-gray-400">Event name</label>
+                        <input type="code" id="code" name="code" className="w-full bg-gray-800 rounded border border-gray-700 mt-4 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label for="event-code" className="leading-7 text-lg text-gray-400">Event code</label>
+                        <input type="event-code" id="event-code" name="event-code" className="w-full bg-gray-800 rounded border mt-4 border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label for="capacity" className="leading-7 text-lg text-gray-400">Capacity</label>
+                        <input type="capacity" id="capacity" name="capacity" className="w-full bg-gray-800 rounded border mt-4 border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label for="radius" className="leading-7 text-lg text-gray-400">Radius</label>
+                        <input type="radius" id="radius" name="radius" className="w-full bg-gray-800 rounded border mt-4 border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div className="relative mb-4">
+                        <label for="message" className="leading-7 text-lg text-gray-400">Check-in Time</label>
+                        {/* <textarea id="message" name="message" className="w-full bg-gray-800 rounded border mt-4 border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea> */}
+                        <input
+                            id="start-time"
+                            type="datetime-local"
+                            name="datetime-picker"
+                            className="w-full bg-gray-800 rounded border mt-4 border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            value={startTime}
+                            onChange={handleStartTimeChange} />
+                        <input
+                            id="end-time"
+                            type="datetime-local"
+                            name="datetime-picker"
+                            className="w-full bg-gray-800 rounded border mt-4 border-gray-700 focus:border-blue-500 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            value={endTime}
+                            onChange={handleEndTimeChange} />
+                    </div>
+                    <button className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Save</button>
+                    <p className="text-xs text-gray-400 text-opacity-90 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+                </div>
+            </div>
+            <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                <table class="table-auto w-full text-left whitespace-no-wrap">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-xl bg-gray-800 rounded-tl rounded-bl">Event Name</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-xl bg-gray-800">Event Code</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-xl bg-gray-800">Event Capacity</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-xl bg-gray-800">Delete Event</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-xl bg-gray-800">Upload Picture</th>
+                            <th class="w-10 title-font tracking-wider font-medium text-white text-sm bg-gray-800 rounded-tr rounded-br"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="px-4 py-3 text-xl text-blue-600 underline cursor-pointer"><a>Name</a></td>
+                            <td class="px-4 py-3 text-xl">5 Mb/s</td>
+                            <td class="px-4 py-3 text-xl">15 GB</td>
+                            <td class="px-4 py-3">
+                                <button onClick={handleSettingsClick}>
+                                    <img className="object-cover object-center rounded" src={deleteIcon} alt="deleteIcon" width={30} />
+                                </button>
+                            </td>
+                            <td class="px-4 py-3">
+                                <button onClick={handleSettingsClick}>
+                                    <img className="object-cover object-center rounded" src={pictureIcon} alt="pictureIcon" width={30} />
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    )
+}
+
+export default Dashboard;
--- dev/frontend/src/components/signup/Signup.js
+++ dev/frontend/src/components/signup/Signup.js
@@ -8,43 +8,43 @@ const Signup = () => {
             <div className="px-5 mx-auto">
                 <div className="flex flex-col text-center w-full">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Sign-up to create an account</h1>
-                    <p className="lg:w-2/3 mx-auto pb-2 leading-relaxed text-xl">Already have an account? <a href=''>Login</a></p>
+                    <p className="lg:w-2/3 mx-auto pb-2 leading-relaxed text-lg">Already have an account? <a class="underline" href='/login'>Login</a></p>
                 </div>
                 <div className="lg:w-1/2 md:w-2/3 mx-auto">
                     <div className="flex flex-wrap -m-2">
                         <div className="p-2 w-full">
                             <div className="relative">
-                                <label for="firstname" className="leading-7 text-xl text-gray-400">First Name</label>
+                                <label for="firstname" className="leading-7 text-md text-gray-400">First Name</label>
                                 <input type="text" id="firstname" name="firstname" className="mt-3 w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
                         <div className="p-2 w-full">
                             <div className="relative">
-                                <label for="name" className="leading-7 text-xl text-gray-400">Last Name</label>
+                                <label for="name" className="leading-7 text-md text-gray-400">Last Name</label>
                                 <input type="text" id="name" name="name" className="mt-3 w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
                         <div className="p-2 w-full">
                             <div className="relative">
-                                <label for="name" className="leading-7 text-xl text-gray-400">Username</label>
+                                <label for="name" className="leading-7 text-md text-gray-400">Username</label>
                                 <input type="text" id="name" name="name" className="mt-3 w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
                         <div className="p-2 w-full">
                             <div className="relative">
-                                <label for="email" className="leading-7 text-xl text-gray-400">Email</label>
+                                <label for="email" className="leading-7 text-md text-gray-400">Email</label>
                                 <input type="email" id="email" name="email" className="mt-3 w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
                         <div className="p-2 w-full">
                             <div className="relative">
-                                <label for="email" className="leading-7 text-xl text-gray-400">Password</label>
+                                <label for="email" className="leading-7 text-md text-gray-400">Password</label>
                                 <input type="email" id="email" name="email" className="mt-3 w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
                         <div className="p-2 w-full">
                             <div className="relative">
-                                <label for="email" className="leading-7 text-xl text-gray-400">Confirm Password</label>
+                                <label for="email" className="leading-7 text-md text-gray-400">Confirm Password</label>
                                 <input type="email" id="email" name="email" className="mt-3 w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
