4ff51955aab23f61f3e591be7595ca05eb2a74ae
Bence Danko <bencejdanko@gmail.com>
2024-04-27 18:08:15 -0700

    update passes to be env variables

--- dev/backend-pb/pb_migrations/initial_admin.js
+++ dev/backend-pb/pb_migrations/initial_admin.js
@@ -2,12 +2,7 @@ migrate((db) => {
     const dao = new Dao(db)
 
     let admin = new Admin()
-    admin.email = "BENCEJDANKO@GMAIL.COM"
-    admin.setPassword('}N;9q$9P&3>l')
-    dao.saveAdmin(admin)
-
-    admin = new Admin()
-    admin.email = "example@example.COM"
-    admin.setPassword('&J$3R-h9*m3u')
+    admin.email = process.env.GEOPB_ADMIN_EMAIL
+    admin.setPassword(process.env.GEOPB_ADMIN_PASSWORD)
     dao.saveAdmin(admin)
 })
