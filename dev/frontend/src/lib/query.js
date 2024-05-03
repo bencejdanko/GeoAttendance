4e644d7fe108042bc7ebc0833b31fb0b2d8d3327
Thao Trinh <dttrinh96@gmail.com>
2024-05-03 11:44:21 -0700

    more data persistance

--- dev/frontend/src/lib/query.js
+++ dev/frontend/src/lib/query.js
@@ -256,7 +256,7 @@ export default {
         try {
             const event = await pb.collection('events').getFullList({
                 filter: `id='${eventId}'`,
-                expand: 'registered_attendees'
+                expand: 'registered_attendees,group_id.registered_attendees'
             })
             return event;
         } catch (e) {
